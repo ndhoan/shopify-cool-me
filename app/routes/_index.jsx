841d21a3a69a69b0c9ea5f660394b51e467a67c7
Hoan Nguyen <ndhoan1995@gmail.com>
2024-02-20 09:03:30 +0700

    update image size

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -31,6 +31,8 @@ export default function Index() {
                     key={collection.id}
                     sizes="(max-width: 32em) 100vw, 33vw"
                     crop="center"
+                    height={'32em'}
+                    width={'32em'}
                   />
                 )}
                 <h2 className="whitespace-pre-wrap max-w-prose font-medium text-copy">
