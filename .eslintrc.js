9a79fa20d4b0071ee3d2c03513d6f97645743754
Hoan Nguyen <ndhoan1995@gmail.com>
2024-03-21 13:32:45 +0700

    make private

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
-  extends: [
-    '@remix-run/eslint-config',
-    'plugin:hydrogen/recommended',
-  ],
+  extends: ['@remix-run/eslint-config', 'plugin:hydrogen/recommended'],
   rules: {
     'hydrogen/prefer-image-component': 'off',
     'no-useless-escape': 'off',
     'no-case-declarations': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
